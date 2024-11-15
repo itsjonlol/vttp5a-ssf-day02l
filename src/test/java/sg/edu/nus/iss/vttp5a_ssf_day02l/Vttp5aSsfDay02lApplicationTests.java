02006d7e38c327f9f757b4449cd0d55894f3b70d
itsjonlol <jonathansuherman@gmail.com>
2024-11-15 09:11:22 +0800

    first commit

---
+++ src/test/java/sg/edu/nus/iss/vttp5a_ssf_day02l/Vttp5aSsfDay02lApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_ssf_day02l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aSsfDay02lApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
